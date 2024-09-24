2d0c31178ccb39a61e3377a20592f905392596df
Simas Bradaitis <simasbra@proton.me>
2024-09-24 12:54:22 +0300

    1.3 print

--- Laboratorinis1/laboratorinis.py
+++ Laboratorinis1/laboratorinis.py
@@ -5,21 +5,23 @@ import matplotlib.pyplot as pyplot
 from scipy.optimize import fsolve
 
 
+# -0.45x^4 + 1.04x^3 + 1.42x^2 - 2.67x - 0.97
 def daugianarisF(x):
     return -0.45 * x**4 + 1.04 * x**3 + 1.42 * x**2 - 2.67 * x - 0.97
 
 
+# cos(2x) / (sin(x) + 1.5)) - (x / 5), kai -5 <= x <= 5
 def funkcijaG(x):
     return (numpy.cos(2 * x) / (numpy.sin(x) + 1.5)) - (x / 5)
 
 
-def grubusIntervalas(koeficientai):
+def grubus_intervalas(koeficientai):
     max_koef = max(koeficientai[:-1], key=abs)
     R = 1 + (max_koef / koeficientai[-1])
     return round(R, 2)
 
 
-def tikslesnisIntervalas(koeficientai):
+def tikslesnis_intervalas(koeficientai):
     n = len(koeficientai)
     max_abs_index, B = min(enumerate(koeficientai[:-1]), key=lambda x: x[1])
     k = n - max_abs_index
@@ -38,6 +40,7 @@ def skenavimas(funkcija, intervalas, zingsnis):
     dabZenklas = False if func(x) < 0 else True
     # kol prieiname intervalo galą tikriname ženklus tarp skirtingų galų
     # jeigu ženklai nesutampa - tame intervale bus šaknis, tad ją ir išsaugome
+    iterations = 0
     while x < intervalas[1]:
         buvZenklas = dabZenklas
         dabZenklas = False if func(x) < 0 else True
@@ -45,11 +48,13 @@ def skenavimas(funkcija, intervalas, zingsnis):
         if buvZenklas != dabZenklas:
             saknys.append((x-zingsnis, x))
         x += zingsnis
-    return saknys
+        iterations += 1
+    return saknys, iterations
 
 
-def pusiaukirtosMetodas(funkcija, saknys, tolerancija=1e-10, iteracijosMax=1000):
+def pusiaukirtos(funkcija, saknys, tolerancija=1e-10, iteracijosMax=1000):
     patikslintosSaknys = []
+    iterations = []
     for saknuPora in saknys:
         xFrom = saknuPora[0]
         xTo = saknuPora[1]
@@ -63,16 +68,19 @@ def pusiaukirtosMetodas(funkcija, saknys, tolerancija=1e-10, iteracijosMax=1000)
                 xTo = xMid
             xMid = (xFrom + xTo) / 2
         patikslintosSaknys.append(xMid)
+        iterations.append(iteration)
         # print(f'Artinys ({xFrom}, {xTo})')
         # print(f'Patikslinta saknis: {xMid} Iteracijos: {format(iteration)}')
-    return patikslintosSaknys
+    return patikslintosSaknys, iterations
 
 
-def kvaziNiutonoMetodas(funkcija, saknys, iteracijosMax=1000):
+def kvazi_niutono(funkcija, saknys, iteracijosMax=1000):
     patikslintosSaknys = []
+    iterations = []
     for saknuPora in saknys:
         x0 = saknuPora[0]
         x1 = saknuPora[1]
+        iteration = 0
         for i in range(iteracijosMax):
             funkcijax0 = funkcija(x0)
             funkcijax1 = funkcija(x1)
@@ -81,8 +89,10 @@ def kvaziNiutonoMetodas(funkcija, saknys, iteracijosMax=1000):
             x2 = x1 - (funkcija(x1) * (x1 - x0) /
                        float(funkcijax1 - funkcijax0))
             x0, x1 = x1, x2
+            iteration += 1
         patikslintosSaknys.append(x2)
-    return patikslintosSaknys
+        iterations.append(iteration)
+    return patikslintosSaknys, iterations
 
 
 koeficientai = [-0.45, 1.04, 1.42, -2.67, -0.97]
@@ -101,12 +111,12 @@ if koeficientai[-1] < 0:
 if koeficientaiNeigX[-1] < 0:
     koeficientaiNeigX = [x * (-1) for x in koeficientaiNeigX]
 
-R_GRUBUS = grubusIntervalas(koeficientai)
+R_GRUBUS = grubus_intervalas(koeficientai)
 grubus = (-R_GRUBUS, R_GRUBUS)
 print(f'"grubus" intervalas: {grubus}')
 
-R_TIKSL_TEIG = tikslesnisIntervalas(koeficientai)
-R_TIKSL_NEIG = tikslesnisIntervalas(koeficientaiNeigX)
+R_TIKSL_TEIG = tikslesnis_intervalas(koeficientai)
+R_TIKSL_NEIG = tikslesnis_intervalas(koeficientaiNeigX)
 tikslesnis = (-R_TIKSL_NEIG, R_TIKSL_TEIG)
 print(f'"tikslesnis" intervalas: {tikslesnis}')
 
@@ -128,12 +138,12 @@ plotF1.set_ylabel("y")
 plotF1.plot(iverciaiF, figureF, "m")
 plotF1.axhline(y=0, lw=1, color='k')
 plotF1.grid()
-grubusIntervalas = plotF1.scatter(
+grubus_intervalas = plotF1.scatter(
     [-R_GRUBUS, R_GRUBUS], [0, 0], marker="v", color="b")
 tiklsIntervalas = plotF1.scatter(
     [-R_TIKSL_NEIG, R_TIKSL_TEIG], [0, 0], marker="*", color="r")
 
-plotF1.legend((grubusIntervalas, tiklsIntervalas),
+plotF1.legend((grubus_intervalas, tiklsIntervalas),
               ('"Grubaus" intervalo galai', '"Tikslesnio" intervalo galai'),
               scatterpoints=1, fontsize=8)
 
@@ -147,12 +157,12 @@ plotF2.set_xlim([-2, 2])
 plotF2.set_ylim([-2, 2])
 plotF2.axhline(y=0, lw=1, color='k')
 plotF2.grid()
-grubusIntervalas = plotF2.scatter(
+grubus_intervalas = plotF2.scatter(
     [-R_GRUBUS, R_GRUBUS], [0, 0], marker="v", color="b")
 tiklsIntervalas = plotF2.scatter(
     [-R_TIKSL_NEIG, R_TIKSL_TEIG], [0, 0], marker="*", color="r")
 
-plotF2.legend((grubusIntervalas, tiklsIntervalas),
+plotF2.legend((grubus_intervalas, tiklsIntervalas),
               ('"Grubaus" intervalo galai', '"Tikslesnio" intervalo galai'),
               scatterpoints=1, fontsize=8)
 pyplot.show()
@@ -192,8 +202,8 @@ pyplot.show()
 
 print("\n1.2")
 zingsnis = 0.1
-daugianarioSaknys = skenavimas('f', galutinis, zingsnis)
-funkcijosSaknys = skenavimas('g', (-5, 5), zingsnis)
+daugianarioSaknys, iterationsSkenF = skenavimas('f', galutinis, zingsnis)
+funkcijosSaknys, iterationsSkenG = skenavimas('g', (-5, 5), zingsnis)
 print(f'Daugianario saknys skenavimo algoritmu: {daugianarioSaknys}')
 print(f'Funkcijos saknys skenavimo algoritmu: {funkcijosSaknys}')
 
@@ -204,19 +214,77 @@ print(f'Funkcijos saknys skenavimo algoritmu: {funkcijosSaknys}')
 
 print("\n1.3")
 # pusiaukirtos metodas
-pusiaukirtosF = pusiaukirtosMetodas(
-    daugianarisF, daugianarioSaknys, 1e-15, 1000)
-print(f'Daugianario saknys pusiaukirtos metodu: {pusiaukirtosF}')
+epsilonF = 1e-15
+pusiaukirtosF, iterationsPusF = pusiaukirtos(
+    daugianarisF, daugianarioSaknys, epsilonF, 1000)
+# print(f'Daugianario saknys pusiaukirtos metodu: {pusiaukirtosF}')
 
-pusiaukirtosG = pusiaukirtosMetodas(funkcijaG, funkcijosSaknys, 1e-14, 1000)
-print(f'Funkcijos saknys pusiaukirtos metodu: {pusiaukirtosG}')
+epsilonG = 1e-14
+pusiaukirtosG, iterationsPusG = pusiaukirtos(
+    funkcijaG, funkcijosSaknys, epsilonG, 1000)
+# print(f'Funkcijos saknys pusiaukirtos metodu: {pusiaukirtosG}')
 
 # kvazi-niutono (kirstiniu) metodas
-kvaziNiutonoF = kvaziNiutonoMetodas(daugianarisF, daugianarioSaknys, 1000)
-print(f'Daugianario saknys Kvazi-Niutono metodu: {kvaziNiutonoF}')
+kvaziNiutonoF, iterationsKvazF = kvazi_niutono(
+    daugianarisF, daugianarioSaknys, 1000)
+# print(f'Daugianario saknys Kvazi-Niutono metodu: {kvaziNiutonoF}')
 
-kvaziNiutonoG = kvaziNiutonoMetodas(funkcijaG, funkcijosSaknys, 1000)
-print(f'Funkcijos saknys Kvazi-Niutono metodu: {kvaziNiutonoG}')
+kvaziNiutonoG, iterationsKvazG = kvazi_niutono(
+    funkcijaG, funkcijosSaknys, 1000)
+# print(f'Funkcijos saknys Kvazi-Niutono metodu: {kvaziNiutonoG}')
+
+# Skaičiavimų rezultatus pateikite lentelėje, kurioje nurodykite šaknies
+# tikslinimui naudojamą metodą, pradinį artinį arba atskyrimo intervalą, gautą
+# sprendinį (šaknį), funkcijos reikšmę ties šaknimi, tikslumą, iteracijų skaičių.
+# Palyginkite, kuriuo metodu sprendiniui rasti panaudota mažiau iteracijų;
+print('-' * 107)
+print("| METODAS".ljust(15) + " | ATSKYRIMO INTERVALAS".ljust(25) +
+      " | GAUTA ŠAKNIS".ljust(20) + " | F. REIKŠMĖ".ljust(20) +
+      " | epsilon".ljust(10) + " | ITERACIJOS".ljust(15) + " |")
+print('-' * 107)
+print("| f(x) = -0.45x^4 + 1.04x^3 + 1.42x^2 - 2.67x - 0.97" + ' ' * 54 + '|')
+print('-' * 107)
+for i in range(len(daugianarioSaknys)):
+    saknuPora = daugianarioSaknys[i]
+    pusiaukirtos = pusiaukirtosF[i]
+    iterationPus = iterationsPusF[i]
+    kvaziNiuntono = kvaziNiutonoF[i]
+    iterationKvaz = iterationsKvazF[i]
+    print("| Pusiaukirtos".ljust(15) +
+          f" | [{round(saknuPora[0], 3)} - {round(saknuPora[1], 3)}]".ljust(25) +
+          f" | {round(pusiaukirtos, 10)}".ljust(20) +
+          f" | {round(daugianarisF(pusiaukirtos), 14)}".ljust(20) +
+          f" | {epsilonF}".ljust(10) +
+          f" | {iterationPus}".ljust(15) + " |")
+    print("| Kvazi-Niutono".ljust(15) +
+          f" | [{round(saknuPora[0], 3)} - {round(saknuPora[1], 3)}]".ljust(25) +
+          f" | {round(kvaziNiuntono, 10)}".ljust(20) +
+          f" | {round(daugianarisF(kvaziNiuntono), 14)}".ljust(20) +
+          f" | {epsilonF}".ljust(10) +
+          f" | {iterationKvaz}".ljust(15) + " |")
+print('-' * 107)
+print("| g(x) = cos(2x) / (sin(x) + 1.5)) - (x / 5), kai -5 <= x <= 5" +
+      ' ' * 44 + '|')
+print('-' * 107)
+for i in range(len(funkcijosSaknys)):
+    saknuPora = funkcijosSaknys[i]
+    pusiaukirtos = pusiaukirtosG[i]
+    iterationPus = iterationsPusG[i]
+    kvaziNiuntono = kvaziNiutonoG[i]
+    iterationKvaz = iterationsKvazG[i]
+    print("| Pusiaukirtos".ljust(15) +
+          f" | [{round(saknuPora[0], 3)} - {round(saknuPora[1], 3)}]".ljust(25) +
+          f" | {round(pusiaukirtos, 10)}".ljust(20) +
+          f" | {round(daugianarisF(pusiaukirtos), 13)}".ljust(20) +
+          f" | {epsilonG}".ljust(10) +
+          f" | {iterationPus}".ljust(15) + " |")
+    print("| Kvazi-Niutono".ljust(15) +
+          f" | [{round(saknuPora[0], 3)} - {round(saknuPora[1], 3)}]".ljust(25) +
+          f" | {round(kvaziNiuntono, 10)}".ljust(20) +
+          f" | {round(daugianarisF(kvaziNiuntono), 13)}".ljust(20) +
+          f" | {epsilonG}".ljust(10) +
+          f" | {iterationKvaz}".ljust(15) + " |")
+print('-' * 107)
 
 # 1.4
 # Gautas šaknų reikšmes patikrinkite naudodami išorinius išteklius
@@ -235,5 +303,5 @@ for saknuPora in funkcijosSaknys:
 print(f'Funkcijos saknys su scipy.optimize.fsolve: {saknysFsolveG}')
 
 # 2.1
-# Pateikite tarpinius grafikus, kai drauge su pateikta funkcija ℎ(𝑥)
+# Pateikite tarpinius grafikus, kai drauge su pateikta funkcija ℎ(𝑥 )
 # nurodytame intervale atvaizduojama TE, kaijos narių skaičius lygus 3, 4 ir 5
